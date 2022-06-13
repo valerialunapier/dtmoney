b4123bfec740996c06bcc1be30d95859e3cbd50c
valeria luna <valeria.luna@pier.digital>
2022-06-13 17:52:45 -0300

    adicao do miragejs e do axios

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles';
 
 export function Header() {
-    return (
-        <Container>
-            <Content>
-                <img src={logoImg} alt="dt money" />
-                <button type="button">
-                    Nova transação
-                </button>
-            </Content>
-        </Container>
-    );
+	const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModal(true);
+	}
+
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModal(false);
+	}
+
+
+	return (
+		<Container>
+			<Content>
+				<img src={logoImg} alt="dt money" />
+				<button type="button" onClick={handleOpenNewTransactionModal}>
+					Nova transação
+				</button>
+			</Content>
+		</Container>
+	);
 }
